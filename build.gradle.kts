9fb31de3e79dc77463355e71f9e1a8bdafd2cbc2
Huzaifa_Alfararjeh <huthefa.alfararjeh@beno.com>
2026-06-08 13:59:00 +0300

    fix: use apply false on root kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
 plugins {
-    kotlin("multiplatform") version "2.3.21"
-}
-
-group = "com.fajrbahr.mediatork"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+    kotlin("multiplatform") version "2.3.21" apply false
 }
