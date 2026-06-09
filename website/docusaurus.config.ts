b7ae9433a0b12e586900e8dc7225f8af9cd0254e
Huzaifa_Alfararjeh <huthefa.alfararjeh@beno.com>
2026-06-09 12:05:04 +0300

    feat: replace favicon with robot icon

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'MediatorK',
   tagline: 'Coroutine-first Mediator for Kotlin & KMP',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   future: {
     v4: true,
