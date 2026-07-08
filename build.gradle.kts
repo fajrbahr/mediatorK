5aa3bd8258ded530579cdc0ccc2aa9e818bf54db
Huzaifa_Alfararjeh <huthefa.alfararjeh@beno.com>
2026-07-08 11:00:53 +0300

    0.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.detekt)
-    alias(libs.plugins.binary.compatibility.validator)
-}
-
-// Guards the public API of the published modules: any signature change fails CI
-// until it is made explicit by re-running `./gradlew apiDump`.
-apiValidation {
-    ignoredProjects += listOf("publishMaven")
 }
 
 detekt {
